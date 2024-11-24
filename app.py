ca10384cfad3f6da64a159a2a1a7c2d09f46d4ff
Subasree Umachandran <subasreeumachandran@gmail.com>
2024-11-24 13:50:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Define a route for the home page
 @app.route("/")
-def hello_world():
-  return "Hello, suba"
-  
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
